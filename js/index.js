dbb6cc7f4349dc525ce71367114a26d81c161beb
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-05-05 18:20:38 -0500

    initial commit+completed some added to index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
-// Your code goes here
+// prevent default of anchor tags
+
+// save the navLinks in a variable
+const navLinks = document.querySelectorAll('.nav-link')
+// create preventDefault script
+const preventD = (e) => e.preventDefault(); 
+// use forEach to add event listener to navLinks
+navLinks.forEach(link => link.addEventListener('click', preventD))
+
+
+
+
+// on click -- change first nav link text
+const firstLink = document.querySelector('.nav-link');
+const changeLinkTxt = () => firstLink.textContent = 'Changed!'
+firstLink.addEventListener('click', changeLinkTxt);
+
+// on mouseover -- change header background 
+const header = document.querySelector('header')
+const changeBC = () => header.style.backgroundColor = 'red';
+header.addEventListener('mouseover', changeBC);
+
+// on mouseout -- change header background back 
+const revertBC = () => (header.style.backgroundColor = 'white');
+header.addEventListener('mouseout', revertBC);
+
+// 
+
+
+
+
