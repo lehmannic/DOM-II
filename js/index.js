8a2fbb5bbca097672a26b090d3bffbb636d4147c
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-05-05 20:04:57 -0500

    finished 5 unique selectors + other 2 challenges

--- js/index.js
+++ js/index.js
@@ -3,28 +3,77 @@
 // save the navLinks in a variable
 const navLinks = document.querySelectorAll('.nav-link')
 // create preventDefault script
-const preventD = (e) => e.preventDefault(); 
+const preventD = e => e.preventDefault(); 
 // use forEach to add event listener to navLinks
 navLinks.forEach(link => link.addEventListener('click', preventD))
 
 
 
+// click on nav-link -- change  nav link text
+const changeLink = e => e.target.textContent = 'changed';
+navLinks.forEach(link => link.addEventListener('click', changeLink)); 
 
-// on click -- change first nav link text
-const firstLink = document.querySelector('.nav-link');
-const changeLinkTxt = () => firstLink.textContent = 'Changed!'
-firstLink.addEventListener('click', changeLinkTxt);
-
-// on mouseover -- change header background 
+// mouseover on header -- change header background 
 const header = document.querySelector('header')
-const changeBC = () => header.style.backgroundColor = 'red';
+const changeBC = () => (header.style.backgroundColor = "#D1826F");
 header.addEventListener('mouseover', changeBC);
 
-// on mouseout -- change header background back 
+// mouseout on header-- change header background back 
 const revertBC = () => (header.style.backgroundColor = 'white');
 header.addEventListener('mouseout', revertBC);
 
-// 
+// dblclick on btn-- alert 'email has been sent'
+const btns = document.querySelectorAll('.btn'); 
+const alertEmail = (e) => {
+    alert('check your email for details on signing up!')
+    e.stopPropagation(); 
+}
+btns.forEach( btn => btn.addEventListener('dblclick', alertEmail));
+
+// ANOTHER SIMILAR EVENT -- dblClick anywhere -- enters full screen --except btn ...see stopProp() above
+
+let doc = document.documentElement; 
+document.body.addEventListener('dblclick', function openFullscreen() {
+    if (document.fullscreen) {
+        document.exitFullscreen(); 
+    } else if (doc.requestFullscreen) {
+        doc.requestFullscreen();
+    } else if (doc.mozRequestFullScreen) {
+        doc.mozRequestFullScreen(); // Firefox
+    } else if (doc.webkitRequestFullscreen) {
+        doc.webkitRequestFullscreen(); ///* Chrome, Safari and Opera */
+    } else if (doc.msRequestFullscreen) {
+        doc.msRequestFullscreen();  // IE, Edge
+    } 
+})
+// used W3Schools and MDN for help on this!
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/fullscreenchange_event
+// https://www.w3schools.com/howto/howto_js_fullscreen.asp
+
+
+// fullscreenchange -- toggles 'dark mode' themes
+document.addEventListener("fullscreenchange", () => {
+    if (document.fullscreenElement) {
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "white";
+        header.style.backgroundColor = 'grey'; 
+    } else {
+                document.body.style.backgroundColor = "white";
+                document.body.style.color = "black";
+                header.style.backgroundColor = "white"; 
+    }
+});
+
+// keydown -- alert if 3 is pressed on keyboard
+document.addEventListener('keydown', e => {
+    if (e.key == 3) {
+        alert(`3 is my favorite number`)
+    }
+})
+
+
+// focus
+
 
 
 
